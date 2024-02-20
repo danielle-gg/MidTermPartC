e192890bb63409798034bdee687cc35dd4c11ac3
danielle-gg <gomezdan@sheridancollege.ca>
2024-02-20 12:10:12 -0300

    created class danielle and added comments

---
+++ src/midtermpartc/danielle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermpartc;
+
+/**
+ *
+ * @author Danielle
+ */
+public class danielle {
+    
+    
+    
+    ///I am adding comments here because I was asked to for the midterm
+    //I hope to do well on this 
+    
+}
