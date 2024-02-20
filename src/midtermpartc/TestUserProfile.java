6bba6cb91decc483f25a577e28b3f8f72d1fa8b5
danielle-gg <gomezdan@sheridancollege.ca>
2024-02-20 12:03:25 -0300

    created TestUserProfile first draft

---
+++ src/midtermpartc/TestUserProfile.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermpartc;
+
+/**
+ *
+ * @author Danielle
+ */
+public class TestUserProfile {
+    
+    public static void main(String[] args) {
+        var name = Scanner input("Please enter your name: ");
+        String[] genres = {"rock", "pop", "rap", "country", "other"};
+        var fav_genre = Scanner input = new Scanner(System.in("Please select your favourite genre " || genres));
+        
+        System.out.println("Your user profile has been created.");
+        
+        //Scanner input = new Scanner(System.in);
+        //System.out.println()
+                
+             
+        
+        
+    }
+    
+}
