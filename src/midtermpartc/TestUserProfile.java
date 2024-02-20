754b5cf69e6186caa9be69033416826cf165eac0
danielle-gg <gomezdan@sheridancollege.ca>
2024-02-20 12:27:19 -0300

    edited code

--- src/midtermpartc/TestUserProfile.java
+++ src/midtermpartc/TestUserProfile.java
@@ -11,15 +11,17 @@ package midtermpartc;
 public class TestUserProfile {
     
     public static void main(String[] args) {
-        var name = Scanner input("Please enter your name: ");
+        Scanner name = System.in("Please enter your name: ");
         String[] genres = {"rock", "pop", "rap", "country", "other"};
-        var fav_genre = Scanner input = new Scanner(System.in("Please select your favourite genre " || genres));
+        var fav_genre = Scanner input = new Scanner(System.in("Please select your favourite genre ", genres));
         
         System.out.println("Your user profile has been created.");
         
         //Scanner input = new Scanner(System.in);
         //System.out.println()
-                
+        
+        
+        
              
         
         
